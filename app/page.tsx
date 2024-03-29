ccd0bfc40c657bee09527f9335f64390350931e3
junaid-56 <junaididrees1111@gmail.com>
2024-03-29 17:24:42 +0500

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <div>hello friends my name is junaid</div>;
+  return (
+    <div>hello friends my name is junaid this is my first NEXT.js project</div>
+  );
 }
